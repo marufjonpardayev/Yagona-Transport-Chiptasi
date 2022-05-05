71b6793382959fe272c99160dc506c58b650e5b3
Perennial2002 <pendragon05082002@gmail.com>
2022-05-05 22:43:48 +0500

    first commit

--- app/src/main/java/uz/transport/yagonatransportchiptasi/ui/activity/MainActivity.kt
+++ app/src/main/java/uz/transport/yagonatransportchiptasi/ui/activity/MainActivity.kt
@@ -1,13 +1,13 @@
-package uz.transport.yagonatransportchiptasi
+package uz.transport.yagonatransportchiptasi.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import uz.transport.yagonatransportchiptasi.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //JUST COMMITTT
     }
 }
