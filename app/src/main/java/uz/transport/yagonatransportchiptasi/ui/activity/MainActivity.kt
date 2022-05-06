a4045f5b08c27fc6705374e9fd43b7600430718f
Perennial2002 <pendragon05082002@gmail.com>
2022-05-06 11:07:41 +0500

    main page done!

--- app/src/main/java/uz/transport/yagonatransportchiptasi/ui/activity/MainActivity.kt
+++ app/src/main/java/uz/transport/yagonatransportchiptasi/ui/activity/MainActivity.kt
@@ -1,7 +1,9 @@
 package uz.transport.yagonatransportchiptasi.ui.activity
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
@@ -10,7 +12,7 @@ import uz.transport.yagonatransportchiptasi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding:ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
     lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +21,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setupUI()
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
     }
 
     private fun setupUI() {
         navController = findNavController(R.id.nav_host_fragment)
-
         binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/uz/transport/yagonatransportchiptasi/ui/fragment/SearchFragment.kt
+++ app/src/main/java/uz/transport/yagonatransportchiptasi/ui/fragment/SearchFragment.kt
@@ -6,15 +6,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import uz.transport.yagonatransportchiptasi.R
+import uz.transport.yagonatransportchiptasi.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
 
+    private lateinit var binding: FragmentSearchBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentSearchBinding.bind(view)
+
+        binding.ivChange.setOnClickListener {
+            changeDestinations()
+        }
+    }
+
+    private fun changeDestinations() {
+        binding.apply {
+            if (tvFrom.text.isNotEmpty() && tvTo.text.isNotEmpty()) {
+                val departure = tvFrom.text
+                tvFrom.text = tvTo.text
+                tvTo.text = departure
+            }
+        }
     }
 }
