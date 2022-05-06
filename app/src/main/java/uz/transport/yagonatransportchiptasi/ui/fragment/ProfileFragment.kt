4fd458bc54d7108473c7e7d4fcb59bfff0fd228f
Perennial2002 <pendragon05082002@gmail.com>
2022-05-06 18:02:59 +0500

    some changes

--- app/src/main/java/uz/transport/yagonatransportchiptasi/ui/fragment/ProfileFragment.kt
+++ app/src/main/java/uz/transport/yagonatransportchiptasi/ui/fragment/ProfileFragment.kt
@@ -1,14 +1,21 @@
 package uz.transport.yagonatransportchiptasi.ui.fragment
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import uz.transport.yagonatransportchiptasi.R
+
 
 class ProfileFragment : Fragment() {
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        requireActivity().window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
--- app/src/main/java/uz/transport/yagonatransportchiptasi/ui/fragment/SearchFragment.kt
+++ app/src/main/java/uz/transport/yagonatransportchiptasi/ui/fragment/SearchFragment.kt
@@ -1,6 +1,5 @@
 package uz.transport.yagonatransportchiptasi.ui.fragment
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import uz.transport.yagonatransportchiptasi.R
 import uz.transport.yagonatransportchiptasi.databinding.FragmentSearchBinding
-import uz.transport.yagonatransportchiptasi.ui.activity.CalendarActivity
 
 class SearchFragment : Fragment() {
 
@@ -27,17 +25,6 @@ class SearchFragment : Fragment() {
 
         binding.ivChange.setOnClickListener {
             changeDestinations()
-        }
-
-        initViews()
-    }
-
-    private fun initViews() {
-        binding.llCalendarDeparture.setOnClickListener {
-            val intent = Intent(requireContext(), CalendarActivity::class.java)
-            intent.putExtra("locationStart", "Toshkent")
-            intent.putExtra("locationEnd", "Moskva")
-            startActivity(intent)
         }
     }
 
