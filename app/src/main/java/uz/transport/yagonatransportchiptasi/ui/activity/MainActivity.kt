833eb261aad0caa28172c824fdc01f9b32b5defb
Perennial2002 <pendragon05082002@gmail.com>
2022-05-06 05:59:27 +0500

    bottomNavigation added with fragments,navigation also added

--- app/src/main/java/uz/transport/yagonatransportchiptasi/ui/activity/MainActivity.kt
+++ app/src/main/java/uz/transport/yagonatransportchiptasi/ui/activity/MainActivity.kt
@@ -2,12 +2,28 @@ package uz.transport.yagonatransportchiptasi.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import uz.transport.yagonatransportchiptasi.R
+import uz.transport.yagonatransportchiptasi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityMainBinding
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        setupUI()
+    }
+
+    private fun setupUI() {
+        navController = findNavController(R.id.nav_host_fragment)
+
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
