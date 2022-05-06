91d15d37991f1b88a28037d48bce9b2805da0dcb
Perennial2002 <pendragon05082002@gmail.com>
2022-05-06 18:04:51 +0500

    some changes

--- app/src/main/java/uz/transport/yagonatransportchiptasi/ui/fragment/ProfileFragment.kt
+++ app/src/main/java/uz/transport/yagonatransportchiptasi/ui/fragment/ProfileFragment.kt
@@ -14,6 +14,7 @@ class ProfileFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        //dark colored status bar
         requireActivity().window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
     }
 
