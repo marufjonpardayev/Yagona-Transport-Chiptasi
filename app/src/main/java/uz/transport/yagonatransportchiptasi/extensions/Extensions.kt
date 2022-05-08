ac8c4a789ba9c710d40a3b2c31e04fdef2f21642
Perennial2002 <pendragon05082002@gmail.com>
2022-05-08 17:22:07 +0500

    add,minus and checkBox status added

---
+++ app/src/main/java/uz/transport/yagonatransportchiptasi/extensions/Extensions.kt
@@ -0,0 +1,31 @@
+package uz.transport.yagonatransportchiptasi.extensions
+
+import android.widget.CheckBox
+import android.widget.TextView
+import uz.transport.yagonatransportchiptasi.R
+import uz.transport.yagonatransportchiptasi.extensions.Extensions.increaseValue
+
+object Extensions {
+    fun TextView.increaseValue(tvMinus: TextView) {
+        this.text = "${this.text.toString().toInt() + 1}"
+        tvMinus.setBackgroundResource(R.drawable.rounded_textview_blue)
+        tvMinus.setTextColor(resources.getColor(R.color.center_color))
+    }
+
+    fun TextView.decreaseValue() {
+        this.text = "${this.text.toString().toInt() - 1}"
+    }
+
+    fun TextView.changeTextColorAndBackground() {
+        this.setBackgroundResource(R.drawable.rounded_textview)
+        this.setTextColor(resources.getColor(R.color.black))
+    }
+
+    fun CheckBox.selectOneAtLeast(checkBox: CheckBox, checkStatus: Boolean) {
+        if (checkBox.isChecked) {
+            this.isChecked = checkStatus
+        } else {
+            this.isChecked = true
+        }
+    }
+}
--- app/src/main/java/uz/transport/yagonatransportchiptasi/ui/fragment/PassengersSetupFragment.kt
+++ app/src/main/java/uz/transport/yagonatransportchiptasi/ui/fragment/PassengersSetupFragment.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import uz.transport.yagonatransportchiptasi.R
 import uz.transport.yagonatransportchiptasi.databinding.FragmentPassengersSetupBinding
-import uz.transport.yagonatransportchiptasi.ui.activity.MainActivity
+import uz.transport.yagonatransportchiptasi.extensions.Extensions.changeTextColorAndBackground
+import uz.transport.yagonatransportchiptasi.extensions.Extensions.decreaseValue
+import uz.transport.yagonatransportchiptasi.extensions.Extensions.increaseValue
+import uz.transport.yagonatransportchiptasi.extensions.Extensions.selectOneAtLeast
 
 
 class PassengersSetupFragment : Fragment() {
@@ -28,8 +32,62 @@ class PassengersSetupFragment : Fragment() {
 
         binding = FragmentPassengersSetupBinding.bind(view)
 
-        binding.btnNext.setOnClickListener {
-            findNavController().navigate(R.id.action_passengersSetupFragment_to_passengerDetailFragment)
+        binding.apply {
+            btnNext.setOnClickListener {
+                openPassengerDetailFragment()
+            }
+
+            tvAddAdult.setOnClickListener {
+                tvAdultsNumber.increaseValue(tvMinusAdult)
+            }
+
+            tvAddChildren.setOnClickListener {
+                tvChildrenNumber.increaseValue(tvMinusChildren)
+            }
+
+            tvAddInfant.setOnClickListener {
+                tvInfantsNumber.increaseValue(tvMinusInfant)
+            }
+
+            tvMinusAdult.setOnClickListener {
+                if (tvAdultsNumber.text.toString().toInt() > 1) {
+                    tvAdultsNumber.decreaseValue()
+                }
+                if (tvAdultsNumber.text.toString().toInt() == 1) {
+                    tvMinusAdult.changeTextColorAndBackground()
+                }
+            }
+
+            tvMinusChildren.setOnClickListener {
+                if (tvChildrenNumber.text.toString().toInt() > 0) {
+                    tvChildrenNumber.decreaseValue()
+                }
+
+                if (tvChildrenNumber.text.toString().toInt() == 0) {
+                    tvMinusChildren.changeTextColorAndBackground()
+                }
+            }
+
+            tvMinusInfant.setOnClickListener {
+                if (tvInfantsNumber.text.toString().toInt() > 0) {
+                    tvInfantsNumber.decreaseValue()
+                }
+                if (tvChildrenNumber.text.toString().toInt() == 0) {
+                    tvMinusInfant.changeTextColorAndBackground()
+                }
+            }
+
+            checkboxDown.setOnCheckedChangeListener { _, b ->
+                checkboxDown.selectOneAtLeast(checkboxUp, b)
+            }
+
+            checkboxUp.setOnCheckedChangeListener { _, b ->
+                checkboxUp.selectOneAtLeast(checkboxDown, b)
+            }
         }
+    }
+
+    private fun openPassengerDetailFragment() {
+        findNavController().navigate(R.id.action_passengersSetupFragment_to_passengerDetailFragment)
     }
 }
