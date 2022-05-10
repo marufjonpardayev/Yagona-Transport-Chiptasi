4ce303792926c5689481f77824c014c485cb1e68
Perennial2002 <pendragon05082002@gmail.com>
2022-05-10 12:10:44 +0500

    ticket view added

--- app/src/main/java/uz/transport/yagonatransportchiptasi/ui/fragment/PaymentFragment.kt
+++ app/src/main/java/uz/transport/yagonatransportchiptasi/ui/fragment/PaymentFragment.kt
@@ -1,20 +1,41 @@
 package uz.transport.yagonatransportchiptasi.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import uz.transport.yagonatransportchiptasi.R
+import uz.transport.yagonatransportchiptasi.databinding.FragmentPaymentBinding
+import uz.transport.yagonatransportchiptasi.ui.activity.TicketActivity
 
 
 class PaymentFragment : Fragment() {
+
+    lateinit var binding: FragmentPaymentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_payment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentPaymentBinding.bind(view)
+
+        binding.btnNext.setOnClickListener {
+            openTicketActivity()
+        }
+    }
+
+    private fun openTicketActivity() {
+        val intent = Intent(requireActivity(),TicketActivity::class.java)
+        startActivity(intent)
     }
 }
