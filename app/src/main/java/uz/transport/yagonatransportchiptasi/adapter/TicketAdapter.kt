01e6dbe1beac7a3053dff021ac782a4706d49b7e
Perennial2002 <pendragon05082002@gmail.com>
2022-05-11 14:29:35 +0500

    action

--- app/src/main/java/uz/transport/yagonatransportchiptasi/adapter/TicketAdapter.kt
+++ app/src/main/java/uz/transport/yagonatransportchiptasi/adapter/TicketAdapter.kt
@@ -9,7 +9,11 @@ import uz.transport.yagonatransportchiptasi.model.Ticket
 import uz.transport.yagonatransportchiptasi.ui.fragment.TicketsFragment
 
 
-class TicketAdapter(val fragment: TicketsFragment, val items: ArrayList<Ticket>):
+class TicketAdapter(
+    val fragment: TicketsFragment,
+    val items: ArrayList<Ticket>,
+    private var onItemClicked: (() -> Unit)
+) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private val TYPE_ITEM_BIZNES = 1
@@ -21,48 +25,52 @@ class TicketAdapter(val fragment: TicketsFragment, val items: ArrayList<Ticket>)
 
     override fun getItemViewType(position: Int): Int {
         val item = items[position]
-        return if (item.type == TYPE_ITEM_BIZNES){
+        return if (item.type == TYPE_ITEM_BIZNES) {
             TYPE_ITEM_BIZNES
-        }else if (item.type == TYPE_ITEM_EKONOM){
+        } else if (item.type == TYPE_ITEM_EKONOM) {
             TYPE_ITEM_EKONOM
-        }else if (item.type == TYPE_ITEM_KUPE){
+        } else if (item.type == TYPE_ITEM_KUPE) {
             TYPE_ITEM_KUPE
-        }else if (item.type == TYPE_ITEM_LYUKS){
+        } else if (item.type == TYPE_ITEM_LYUKS) {
             TYPE_ITEM_LYUKS
-        }else if (item.type == TYPE_ITEM_PLATSKART){
+        } else if (item.type == TYPE_ITEM_PLATSKART) {
             TYPE_ITEM_PLATSKART
-        } else{
+        } else {
             TYPE_ITEM_VIP
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        if (viewType == TYPE_ITEM_BIZNES){
+        if (viewType == TYPE_ITEM_BIZNES) {
             val view =
                 ItemTicketBiznesBinding.inflate(LayoutInflater.from(parent.context), parent, false)
             return BiznesViewHolder(view)
         }
 
-        if (viewType == TYPE_ITEM_EKONOM){
+        if (viewType == TYPE_ITEM_EKONOM) {
             val view =
                 ItemTicketEkonomBinding.inflate(LayoutInflater.from(parent.context), parent, false)
             return EkonomViewHolder(view)
         }
 
-        if (viewType == TYPE_ITEM_KUPE){
+        if (viewType == TYPE_ITEM_KUPE) {
             val view =
                 ItemTicketKupeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
             return KupeViewHolder(view)
         }
 
-        if (viewType == TYPE_ITEM_LYUKS){
+        if (viewType == TYPE_ITEM_LYUKS) {
             val view =
                 ItemTicketLyuksBinding.inflate(LayoutInflater.from(parent.context), parent, false)
             return LyuksViewHolder(view)
         }
         if (viewType == TYPE_ITEM_PLATSKART) {
             val view =
-                ItemTicketPlatskartBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                ItemTicketPlatskartBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
             return PlatskartViewHolder(view)
         }
 
@@ -72,34 +80,57 @@ class TicketAdapter(val fragment: TicketsFragment, val items: ArrayList<Ticket>)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is BiznesViewHolder) {
+            holder.view.llBiznes.setOnClickListener {
+                onItemClicked.invoke()
+            }
+        }
 
+        if (holder is EkonomViewHolder) {
+            holder.view.llEkonom.setOnClickListener {
+                onItemClicked.invoke()
+            }
+        }
+
+        if (holder is KupeViewHolder) {
+            holder.view.llKupe.setOnClickListener {
+                onItemClicked.invoke()
+            }
+        }
+
+        if (holder is LyuksViewHolder) {
+            holder.view.llLyuks.setOnClickListener {
+                onItemClicked.invoke()
+            }
+        }
+
+        if (holder is PlatskartViewHolder) {
+            holder.view.llPlaskart.setOnClickListener {
+                onItemClicked.invoke()
+            }
+        }
+
+        if (holder is VipViewHolder) {
+            holder.view.llVip.setOnClickListener {
+                onItemClicked.invoke()
+            }
+        }
     }
 
     override fun getItemCount(): Int {
         return items.size
     }
 
-    class BiznesViewHolder(val view: ItemTicketBiznesBinding) : RecyclerView.ViewHolder(view.root) {
+    class BiznesViewHolder(val view: ItemTicketBiznesBinding) : RecyclerView.ViewHolder(view.root)
 
-    }
+    class EkonomViewHolder(val view: ItemTicketEkonomBinding) : RecyclerView.ViewHolder(view.root)
 
-    class EkonomViewHolder(val view: ItemTicketEkonomBinding) : RecyclerView.ViewHolder(view.root) {
+    class KupeViewHolder(val view: ItemTicketKupeBinding) : RecyclerView.ViewHolder(view.root)
 
-    }
+    class LyuksViewHolder(val view: ItemTicketLyuksBinding) : RecyclerView.ViewHolder(view.root)
 
-    class KupeViewHolder(val view: ItemTicketKupeBinding) : RecyclerView.ViewHolder(view.root) {
+    class PlatskartViewHolder(val view: ItemTicketPlatskartBinding) :
+        RecyclerView.ViewHolder(view.root)
 
-    }
-
-    class LyuksViewHolder(val view: ItemTicketLyuksBinding) : RecyclerView.ViewHolder(view.root) {
-
-    }
-
-    class PlatskartViewHolder(val view: ItemTicketPlatskartBinding) : RecyclerView.ViewHolder(view.root) {
-
-    }
-
-    class VipViewHolder(val view: ItemTicketVipBinding) : RecyclerView.ViewHolder(view.root) {
-
-    }
+    class VipViewHolder(val view: ItemTicketVipBinding) : RecyclerView.ViewHolder(view.root)
 }
